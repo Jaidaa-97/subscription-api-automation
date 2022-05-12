cf398274ba8fa92f247f2dd46fa7022f8f503f04
Jitendra Pisal <jitendra.pisal@fabric.inc>
2022-05-12 12:00:42 +0530

    added report.sh file

---
+++ report.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PROJECT_NAME=$1
+JOB_ID=$2
+PAGES_URL=$3
+env=$4
+service=$5
+test_type=$6
+pipeline_URL=$7
+
+find=".io"
+replaceWith=".io\/-"
+newUrl=$(echo "$PAGES_URL" | sed "s/$find/$replaceWith/")
+newUrl="$newUrl/-/jobs/$JOB_ID/artifacts/target/site/allure-maven-plugin/index.html"
+
+message="In $PROJECT_NAME for pipeline-$pipeline_URL environment-$env, service-$service, $test_type test results are $newUrl"
+curl --location --request POST 'https://hooks.slack.com/services/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' \
+--header 'Content-Type: application/json' \
+--data-raw "{ \"text\": \"$message\", \"channel\": \"oms-qa-allure-reports\"}"
